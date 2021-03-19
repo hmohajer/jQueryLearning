1d9e61ecccb4facb51b111872212f71cb182d822
Hassan <mohajer.hs@gmail.com>
2021-03-19 21:41:17 +0100

    js seprated

--- jQueryBonanza/main.js
+++ jQueryBonanza/main.js
@@ -1,19 +1,22 @@
 $(document).ready(() => {
+    $("#fadeOut").on("click", () => {
+        $("#paraFade").fadeToggle(500, () =>{
+            $("#fadeOut").addClass("w-50");
+        }); 
+    });
+    $("#fadeOut").on("mouseenter" , function(){
+        $("#fadeOut").removeClass("w-50");
+    });
+    $("#append").on("click", () => {
+        $("#listAppend").append("<li class='list-group-item'>"+$("#nameInput").val()+" : "+$("#jobInput").val()+"</li>");
+    });
+    $("#showHide").on("click", () =>{
+        $("#nameInput").fadeToggle(400);
+        $("#jobInput").fadeToggle(400);
+    });
+    $("#changeColor").on("mousemove", () =>{
+        $("#paraColor").css("color", "#"+Math.floor(Math.random() * 1000)+111)
+        $("#paraColor").css("background", "#"+Math.floor(Math.random() * 1000)+111)
+    });
 
-    
 });
-
-
-
-// fadeOut
-// append
-// changeColor
-
-{/* <button id="fadeOut" class="btn btn-dark">Fade Out</button>
-<p id="paraFade" class="p-5">the text should fade out</p>
-
-<button id="append" class="btn btn-dark">Append</button>
-<p id="paraAppend" class="p-5">Append some text here</p>
-
-<button id="changeColor" class="btn btn-dark">Change Color</button>
-<p id="paraColor" class="p-5">change the text color to red</p> */}
