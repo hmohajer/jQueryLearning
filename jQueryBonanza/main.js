310d8a392bc19dc43a2b5aa2a6bc3fa2a08b05f2
Hassan <mohajer.hs@gmail.com>
2021-03-19 21:38:36 +0100

    jQuery asign1

---
+++ jQueryBonanza/main.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+
+    
+});
+
+
+
+// fadeOut
+// append
+// changeColor
+
+{/* <button id="fadeOut" class="btn btn-dark">Fade Out</button>
+<p id="paraFade" class="p-5">the text should fade out</p>
+
+<button id="append" class="btn btn-dark">Append</button>
+<p id="paraAppend" class="p-5">Append some text here</p>
+
+<button id="changeColor" class="btn btn-dark">Change Color</button>
+<p id="paraColor" class="p-5">change the text color to red</p> */}
